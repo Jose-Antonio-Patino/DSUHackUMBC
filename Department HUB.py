17c9de5d619b4f62dba129884882edb29e5b9707
jalejandre2117 <38569746+jalejandre2117@users.noreply.github.com>
2018-10-20 16:26:39 -0400

    Add files via upload

---
+++ Department HUB.py
@@ -0,0 +1,43 @@
+"""
+***
+Stephon Fonrose
+Jorge Alejandre
+Alex Trejo
+Antonio Patino
+
+10/20/18
+v1.2
+***
+"""
+
+
+class Department :
+    
+
+
+    
+class Events(Department) :
+    
+class Department Talks(Events) :
+
+class Tutoring Meeting(Events) :
+
+class Student Events(Events) :
+
+
+    
+    
+class User(Department) :
+
+class Student(User) :
+    
+class Tutors(Student) :
+
+
+    
+    
+class Faculty(User) :
+
+class Administrator(Faculty) :
+
+class Professor(Faculty) :
